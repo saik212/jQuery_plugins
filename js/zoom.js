0676d68aff6ab9a1387a92768f9e64451ae5c9ad
Sai Kandala <kandala.sai@gmail.com>
2015-04-30 22:47:49 -0400

    Adds magnifying glass; magnified image needs proper centering

--- js/zoom.js
+++ js/zoom.js
@@ -4,51 +4,44 @@ $.Zoom = function (el) {
 	this.$height = this.$el.height();
 
 	// Get image to be magnified
-	this.$image = this.$el.children('.zoom-pic');
+	this.$image = this.$el.children('.small-pic');
 
 	// Get lens element
-	this.$lens = this.$el.children('.mag-lens');
+	this.$lens = this.$el.children('.lens');
+	// Get the lens image
+	// this.$zoomImage = this.$lens.children('.zoom-pic');
+	this.$el.on("mousemove", this.magnify.bind(this));
+	// this.$el.on("mouseleave", '.small-pic', this.leaveZoom.bind(this));
+};
 
-	this.$el.on("mousemove", ".zoom-pic", this.magnify.bind(this));
+$.Zoom.prototype.leaveZoom = function (event) {
+	this.$lens.fadeOut(100);
 };
 
 $.Zoom.prototype.magnify = function (event) {
 	event.preventDefault();
 
-	// if (!this.imgWidth && this.imgHeight) {
-		var lensImg = new Image();
-		lensImg.src = this.$image.attr('src');
-		this.$lens.append(lensImg);
+	// Image offset
+	var offset = this.$el.offset();
 
-		this.imgWidth = lensImg.width;
-		this.imgHeight = lensImg.height;
-	// } else {
-		var lensOffset = this.$el.offset();
+	// Grab mouse coordinates
+	var mX = event.pageX;
+	var mY = event.pageY;
 
-		var mX = event.pageX - lensOffset.left;
-		var mY = event.pageY - lensOffset.top;
-		
-		if ( (mX < this.$width) && (mY < this.$height) && (mX > 0) && (mY > 0) ) {
-			this.$lens.fadeIn(100);
-		} else {
-			this.$lens.fadeOut(100);
-		}
-
-		if (this.$lens.is(':visible')){
-			var rX = Math.round( mX/this.$image.width()*this.imgWidth - this.$lens.width()/2 ) -1;
-			var rY = Math.round( mY/this.$image.height()*this.imgHeight - this.$lens.height()/2 ) -1;
-			var bgp = rX +"px " + rY +"px";
-
-			var pX = mX - this.$lens.width()/2;
-			var pY = mY - this.$lens.height()/2;
-		}
-	
-		this.$lens.css({
-			left: pX,
-			top: pY,
-			backgroundPosition: bgp
-		});
-
+	// Mouse offsets
+	var mXOff = mX - (this.$lens.width()/2);
+	var mYOff = mY - (this.$lens.height()/2);
+	// Center magnifier element at mouse coordinates
+	// Allow magnifier element to movie with mouse
+	this.$lens.css({
+		"left": mXOff,
+		"top": mYOff,
+		"background-image": "url(" + this.$image.attr('src') + ")",
+		"background-size": (1.5 * this.$image.width()) + "px " + (1.5 * this.$image.height()) + "px",
+		"background-position": "-" + (1.0 * mXOff) + "px -" + (1.0 * mYOff) + "px",
+		// "background-position": "-" + mXOff + "px -" + mYOff + "px",
+		"background-repeat": "no-repeat"
+	});
 };
 
 $.fn.zoom = function () {
